422252b55666bd453454483beecdbd7a403f5396
Jonathan Halterman <jhalterman@gmail.com>
2021-11-25 08:22:53 -0800

    Rename issue 215 test

--- src/test/java/dev/failsafe/issues/Issue215Test.java
+++ src/test/java/dev/failsafe/issues/Issue215Test.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 @Test
-public class Issue215 {
+public class Issue215Test {
   public void test() {
     RetryPolicy.builder()
       .withBackoff(Duration.ofNanos(Long.MAX_VALUE).minusSeconds(1), Duration.ofSeconds(Long.MAX_VALUE), 1.50);
