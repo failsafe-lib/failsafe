68e6c1f7f44e884f16d6cb0a5eb0aab04384a9df
Jonathan Halterman <jhalterman@gmail.com>
2022-02-09 15:57:38 -0800

    Fix flaky tests

--- src/test/java/dev/failsafe/issues/Issue231Test.java
+++ src/test/java/dev/failsafe/issues/Issue231Test.java
@@ -8,6 +8,8 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.testng.Assert.assertTrue;
@@ -15,12 +17,14 @@ import static org.testng.Assert.assertTrue;
 @Test
 public class Issue231Test {
   /**
-   * Timeout, even with interruption, should wait for the execution to complete.
+   * Timeout, even with interruption, should wait for the execution to complete before completing the future.
    */
   public void shouldWaitForExecutionCompletion() {
+    // Use a separate executorService for this test in case the common pool is full
+    ExecutorService executorService = Executors.newFixedThreadPool(2);
     Timeout<Object> timeout = Timeout.builder(Duration.ofMillis(100)).withInterrupt().build();
     AtomicBoolean executionCompleted = new AtomicBoolean();
-    Asserts.assertThrows(() -> Failsafe.with(timeout).runAsync(() -> {
+    Asserts.assertThrows(() -> Failsafe.with(timeout).with(executorService).runAsync(() -> {
       try {
         Thread.sleep(1000);
       } catch (InterruptedException ignore) {
